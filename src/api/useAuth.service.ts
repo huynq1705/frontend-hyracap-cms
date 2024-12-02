f71d979b728554fe9a8842472eec4dba7fb6e321
Mai Ly Hai <mailyhai814@gmail.com>
2024-12-02 13:31:34 +0700

    fix create blog, login, group sale history

--- src/api/useAuth.service.ts
+++ src/api/useAuth.service.ts
@@ -7,130 +7,129 @@ import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 
 type ResultAuth = {
-    signIn: (payload: SignInPayload) => Promise<SignInResponse>;
-    signUp: (payload: RegisterPayload) => Promise<SignUpResponse>;
-    confirmOtp: (payload: any) => Promise<SignUpResponse>;
+  signIn: (payload: SignInPayload) => Promise<SignInResponse>;
+  signUp: (payload: RegisterPayload) => Promise<SignUpResponse>;
+  confirmOtp: (payload: any) => Promise<SignUpResponse>;
 };
 
 export default function useAuthService(): ResultAuth {
-    const httpClient = useHttpClient();
-    const { axiosBase } = httpClient;
-    const dispatch = useDispatch();
-    const isEmail = (account: string) => {
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailPattern.test(account);
+  const httpClient = useHttpClient();
+  const { axiosBase } = httpClient;
+  const dispatch = useDispatch();
+  const isEmail = (account: string) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(account);
+  };
+
+  const isPhoneNumber = (value: string) => {
+    const phonePattern = /^(0[1-9]{1}[0-9]{8}|[1-9]{1}[0-9]{9,10})$/;
+    return phonePattern.test(value);
+  };
+
+  const generateSignal = async (requestBody: any, secretKey: any) => {
+    const encoder = new TextEncoder();
+    const keyData = encoder.encode(secretKey);
+    const bodyData = encoder.encode(requestBody);
+
+    const key = await crypto.subtle.importKey(
+      "raw",
+      keyData,
+      { name: "HMAC", hash: "SHA-256" },
+      false,
+      ["sign"]
+    );
+
+    const signature = await crypto.subtle.sign("HMAC", key, bodyData);
+    const base64Signature = btoa(
+      String.fromCharCode(...new Uint8Array(signature))
+    );
+
+    return base64Signature;
+  };
+  const signIn = async (payload: any): Promise<any> => {
+    const { email, password, otp } = payload;
+
+    const Body = {
+      // path: "/v2/api/account/login",
+      // method: "POST",
+      // body: {
+      ...(isEmail(email) ? { email: email } : { phone: email }),
+      password,
+      // },
     };
-
-    const isPhoneNumber = (value: string) => {
-        const phonePattern = /^(0[1-9]{1}[0-9]{8}|[1-9]{1}[0-9]{9,10})$/;
-        return phonePattern.test(value);
-    };
-
-    const generateSignal = async (requestBody: any, secretKey: any) => {
-        const encoder = new TextEncoder();
-        const keyData = encoder.encode(secretKey);
-        const bodyData = encoder.encode(requestBody);
-
-        const key = await crypto.subtle.importKey(
-            "raw",
-            keyData,
-            { name: "HMAC", hash: "SHA-256" },
-            false,
-            ["sign"]
-        );
-
-        const signature = await crypto.subtle.sign("HMAC", key, bodyData);
-        const base64Signature = btoa(
-            String.fromCharCode(...new Uint8Array(signature))
-        );
-
-        return base64Signature;
-    };
-    const signIn = async (payload: any): Promise<any> => {
-        const { email, password, otp } = payload;
-
-        const Body = {
-            path: "/v2/api/account/login",
-            method: "POST",
-            body: {
-                ...(isEmail(email) ? { email: email } : { phone: email }),
-                password,
-            },
-        };
-        try {
-            const response = await axios.post(
-                "https://hyracap.lyhai.id.vn/api/account",
-                Body,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            return response.data;
-        } catch (err) {
-            console.error("Error:", err);
-            throw err;
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_APP_BASE_API_URL}staff/login`,
+        // "https://hyracap.lyhai.id.vn/api/account",
+        Body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+      return response.data;
+    } catch (err) {
+      console.error("Error:", err);
+      throw err;
+    }
+  };
+
+  const signUp = async (payload: RegisterPayload): Promise<SignUpResponse> => {
+    const { account, password } = payload;
+
+    const Body = {
+      path: "/v2/api/account/register",
+      method: "POST",
+      body: {
+        ...(isEmail(account) ? { email: account } : { phone: account }),
+        password,
+      },
     };
-
-    const signUp = async (
-        payload: RegisterPayload
-    ): Promise<SignUpResponse> => {
-        const { account, password } = payload;
-
-        const Body = {
-            path: "/v2/api/account/register",
-            method: "POST",
-            body: {
-                ...(isEmail(account) ? { email: account } : { phone: account }),
-                password,
-            },
-        };
-        console.log("payload", payload);
-        try {
-            const response = await axios.post(
-                "https://hyracap.lyhai.id.vn/api/account",
-                Body,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            return response.data;
-        } catch (err) {
-            console.error("Error:", err);
-            throw err;
+    console.log("payload", payload);
+    try {
+      const response = await axios.post(
+        "https://hyracap.lyhai.id.vn/api/account",
+        Body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    };
+      );
+      return response.data;
+    } catch (err) {
+      console.error("Error:", err);
+      throw err;
+    }
+  };
 
-    const confirmOtp = async (payload: any): Promise<any> => {
-        const { email, otp } = payload;
-        const Body = {
-            ...(isEmail(email) ? { email: email } : { phone: email }),
-            verifyCode: otp,
-        };
-        try {
-            const response = await axios.post(
-                "https://hyracap.lyhai.id.vn/api/account",
-                Body,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            return response.data;
-        } catch (err) {
-            console.error("Error:", err);
-            throw err;
+  const confirmOtp = async (payload: any): Promise<any> => {
+    const { email, otp } = payload;
+    const Body = {
+      ...(isEmail(email) ? { email: email } : { phone: email }),
+      verifyCode: otp,
+    };
+    try {
+      const response = await axios.post(
+        "https://hyracap.lyhai.id.vn/api/account",
+        Body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    };
+      );
+      return response.data;
+    } catch (err) {
+      console.error("Error:", err);
+      throw err;
+    }
+  };
 
-    return {
-        signIn,
-        signUp,
-        confirmOtp,
-    };
+  return {
+    signIn,
+    signUp,
+    confirmOtp,
+  };
 }
--- src/api/useSignIn.ts
+++ src/api/useSignIn.ts
@@ -13,101 +13,102 @@ import { setGlobalNoti } from "@/redux/slices/app.slice";
 import { setUserInfo } from "@/redux/slices/user.slice";
 
 type ResultSignIn = {
-    signIn: (formValues: SignInPayload) => Promise<void>;
-    signInWithOTP: (formValues: SignInPayload) => void;
-    isTwoFactorAuth: boolean | undefined;
-    isLoading: boolean;
+  signIn: (formValues: SignInPayload) => Promise<void>;
+  signInWithOTP: (formValues: SignInPayload) => void;
+  isTwoFactorAuth: boolean | undefined;
+  isLoading: boolean;
 };
 
 export default function useSigIn(
-    redirectPath: string | null,
-    onLoginSuccess: (response: any) => void
+  redirectPath: string | null,
+  onLoginSuccess: (response: any) => void
 ): ResultSignIn {
-    const authService = useAuthService();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [isTwoFactorAuth, setTwoFactorAuth] = useState<boolean | undefined>(
-        undefined
-    );
-    const { T } = useCustomTranslation();
+  const authService = useAuthService();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isTwoFactorAuth, setTwoFactorAuth] = useState<boolean | undefined>(
+    undefined
+  );
+  const { T } = useCustomTranslation();
 
-    const mutationSignIn = useMutation({
-        mutationFn: (formValues: SignInPayload) =>
-            authService.signIn(formValues),
-        onSuccess: (res: any) => {
-            onLoginSuccess(res);
-            if (res.status) {
-                const { accessToken } = res.data;
-                const { refreshToken } = res.data;
-                localStorage.setItem(AppConfig.ACCESS_TOKEN, accessToken);
-                localStorage.setItem(AppConfig.REFRESH_TOKEN, refreshToken);
-                dispatch(
-                    setGlobalNoti({
-                        type: "success",
-                        message: "Đăng nhập thành công",
-                    })
-                );
-                window.setTimeout(() => {
-                    if (redirectPath) {
-                        navigate(redirectPath);
-                    } else {
-                        navigate("/admin");
-                    }
-                }, 100);
-            } else {
-                dispatch(
-                    setGlobalNoti({
-                        type: "error",
-                        message: `${res.error}`,
-                    })
-                );
-            }
-        },
-        onError: (err: AxiosError<SignInError>) => {
-            const errResponse = err.response?.data;
-            const message =
-                errResponse?.errorCode || errResponse?.errorMessage
-                    ? `${errResponse.errorCode} - ${errResponse.errorMessage}`
-                    : _.isString(errResponse)
-                    ? errResponse
-                    : T("thereWasError");
-            setGlobalNoti({
-                type: "error",
-                message: `${err.response?.status}: ${message}`,
-                autoHideDuration: null,
-            });
-        },
-        mutationKey: ["signIn"],
-    });
+  const mutationSignIn = useMutation({
+    mutationFn: (formValues: SignInPayload) => authService.signIn(formValues),
+    onSuccess: (res: any) => {
+      onLoginSuccess(res);
+      if (res.status) {
+        const { access_token } = res.data;
+        const { refresh_token } = res.data;
+        // console.log("accessToken", res.data);
 
-    const signIn = async (formValues: SignInPayload) => {
-        const { email } = formValues;
+        localStorage.setItem(AppConfig.ACCESS_TOKEN, access_token);
+        localStorage.setItem(AppConfig.REFRESH_TOKEN, refresh_token);
+        dispatch(
+          setGlobalNoti({
+            type: "success",
+            message: "Đăng nhập thành công",
+          })
+        );
+        window.setTimeout(() => {
+          if (redirectPath) {
+            navigate(redirectPath);
+          } else {
+            navigate("/admin");
+          }
+        }, 100);
+      } else {
+        dispatch(
+          setGlobalNoti({
+            type: "error",
+            message: `${res.error}`,
+          })
+        );
+      }
+    },
+    onError: (err: AxiosError<SignInError>) => {
+      const errResponse = err.response?.data;
+      const message =
+        errResponse?.errorCode || errResponse?.errorMessage
+          ? `${errResponse.errorCode} - ${errResponse.errorMessage}`
+          : _.isString(errResponse)
+          ? errResponse
+          : T("thereWasError");
+      setGlobalNoti({
+        type: "error",
+        message: `${err.response?.status}: ${message}`,
+        autoHideDuration: null,
+      });
+    },
+    mutationKey: ["signIn"],
+  });
 
-        switch (isTwoFactorAuth) {
-            case false:
-            case undefined: {
-                const isUserTwoFactor = false;
+  const signIn = async (formValues: SignInPayload) => {
+    const { email } = formValues;
 
-                setTwoFactorAuth(isUserTwoFactor);
-                if (!isUserTwoFactor) {
-                    mutationSignIn.mutateAsync(formValues);
-                }
-            }
+    switch (isTwoFactorAuth) {
+      case false:
+      case undefined: {
+        const isUserTwoFactor = false;
+
+        setTwoFactorAuth(isUserTwoFactor);
+        if (!isUserTwoFactor) {
+          mutationSignIn.mutateAsync(formValues);
         }
-    };
+      }
+    }
+  };
 
-    const signInWithOTP = (formValues: SignInPayload) => {
-        mutationSignIn.mutateAsync(formValues);
-    };
+  const signInWithOTP = (formValues: SignInPayload) => {
+    mutationSignIn.mutateAsync(formValues);
+  };
 
-    const isLoading = useMemo(() => {
-        return mutationSignIn.isLoading;
-    }, [mutationSignIn.isLoading]);
+  const isLoading = useMemo(() => {
+    return mutationSignIn.isLoading;
+  }, [mutationSignIn.isLoading]);
 
-    return {
-        signIn,
-        signInWithOTP,
-        isTwoFactorAuth,
-        isLoading,
-    };
+  return {
+    signIn,
+    signInWithOTP,
+    isTwoFactorAuth,
+    isLoading,
+  };
 }
